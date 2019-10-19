1685486adc91dfe036fde1f49a948f06f7b54dc1
Eva van Geenen <eva.vangeenen@incentro.com>
2019-10-19 18:40:31 +0200

    Added @Before aspect

--- src/com/luv2code/aopdemo/dao/AccountDAO.java
+++ src/com/luv2code/aopdemo/dao/AccountDAO.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Component;
 public class AccountDAO {
 
 	public void addAccount() {
-		System.out.println(getClass() + "Creating an account in the database...");
+		System.out.println(getClass() + " Creating an account in the database...");
 	}
 	
 }
